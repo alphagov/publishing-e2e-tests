2888d623e0130ed3fdf0d843683f167581ecfc5d
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-03-10 08:56:51 +0000

    Remove waiting on elasticsearch
    By changing the order with which these commands are run.

--- start-docker.sh
+++ start-docker.sh
@@ -3,21 +3,13 @@ set -e
 
 docker-compose down
 docker-compose build
-
-# Elastic Search takes about 10 seconds to be available after the container has
-# started. When HEALTHCHECK (https://docs.docker.com/engine/reference/builder/#healthcheck)
-# is available on ElasticSearch we can use that instead.
 docker-compose up -d elasticsearch
-set +e
-wget --retry-connrefused --tries=10 -q --wait=3 --spider http://localhost:9200
-set -e
-
-docker-compose run -e RUMMAGER_INDEX=all rummager bundle exec rake rummager:migrate_index
 docker-compose run router-api bundle exec rake db:purge
 docker-compose run draft-router-api bundle exec rake db:purge
 docker-compose run content-store bundle exec rake db:purge
 docker-compose run draft-content-store bundle exec rake db:purge
 docker-compose run publishing-api bundle exec rake db:setup
+docker-compose run -e RUMMAGER_INDEX=all rummager bundle exec rake rummager:migrate_index
 docker-compose run publishing-api-worker rails runner 'Sidekiq::Queue.new.clear'
 docker-compose run publishing-api-worker bundle exec rails runner 'channel = Bunny.new.start.create_channel;Bunny::Exchange.new(channel, :topic, "test")'
 docker-compose run specialist-publisher bundle exec rake db:seed
