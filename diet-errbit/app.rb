ef5df8347824a07204774c6897b76f654f879fac
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-03-27 17:07:42 +0100

    Diet Errbit
    This is a super stripped down endpoint for Airbrake errors and acts as
    a replacement for errbit in end-to-end tests.
    
    It's a basic sinatra app that logs errors to STDOUT and to log files
    that are used in the build output.

---
+++ diet-errbit/app.rb
@@ -0,0 +1,44 @@
+require "sinatra"
+require "sinatra/json"
+require "json"
+require "yaml"
+
+ERROR_LOG = ENV.fetch("ERROR_LOG", "/app/tmp/errors.log")
+VERBOSE_ERROR_LOG = ENV.fetch("ERROR_LOG", "/app/tmp/errors-verbose.log")
+
+get "/" do
+  json greeting: "Hello, this is a quick lo-fi alternative to errbit for logging errors to a file."
+end
+
+post "/api/v3/projects/:project_id/notices" do
+  error = JSON.parse(request.body.read.to_s.force_encoding("UTF-8"))
+
+  log_error(error)
+  write_error_to_file(error)
+  write_verbose_error_to_file(error)
+
+  # Respond what airbrake is expecting
+  status 201
+  json id: 1, url: ""
+end
+
+def log_error(error)
+  error["errors"].each { |e| logger.info("#{e['type']} #{e['message']}") }
+end
+
+def write_error_to_file(error)
+  abridged_error = {
+    timestamp: Time.now,
+    errors: error["errors"].map do |e|
+      message = (e["message"] || "")[0...150]
+      { type: e["type"], message: message }
+    end,
+    context: error["context"],
+    }
+  File.open(ERROR_LOG, "a") { |f| f << abridged_error.to_yaml }
+end
+
+def write_verbose_error_to_file(error)
+  error = { "timestamp" => Time.now }.merge(error) unless error["timestamp"]
+  File.open(VERBOSE_ERROR_LOG, "a") { |f| f << error.to_yaml }
+end
