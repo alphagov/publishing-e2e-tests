bdfb894b90dec8f8933bc3f280173ba981865056
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-03-30 20:27:28 +0100

    Make ReloadPageHelpers more generic
    Added in capabilities to check files return 200

--- spec/support/retry_helpers.rb
+++ spec/support/retry_helpers.rb
@@ -1,17 +1,7 @@
-module ReloadPageHelpers
-  class TimeoutError < RuntimeError; end
+require "httparty"
 
-  def reload_page_while_failing
-    attempt = 1
-    begin
-      yield
-    rescue Capybara::ElementNotFound, RSpec::Expectations::ExpectationNotMetError => e
-      raise e if attempt >= RSpec.configuration.reload_page_attempts
-      attempt += 1
-      visit(page.current_url)
-      retry
-    end
-  end
+module RetryHelpers
+  class TimeoutError < RuntimeError; end
 
   def reload_page_until(capybara_method, value, options = {})
     reload_options = {
@@ -60,20 +50,56 @@ module ReloadPageHelpers
     end
   end
 
+  def reload_page_while_false(fail_reason: nil, reload_seconds: nil, interval_seconds: nil, &block)
+    fail_reason ||= "#{page.current_url} was not passing the expectation."
+    retry_while_false(
+      attempt_method: block,
+      retry_method: -> { visit(page.current_url) },
+      fail_reason: fail_reason,
+      reload_seconds: reload_seconds,
+      interval_seconds: interval_seconds,
+    )
+  end
+
+  def reload_url_until_status_code(
+    url,
+    status_code,
+    keep_retrying_while: [404],
+    reload_seconds: nil,
+    interval_seconds: nil
+  )
+    status_codes = Array(status_code)
+    attempt_method = -> do
+      code = HTTParty.head(url).code
+      unless (keep_retrying_while + status_codes).include?(code)
+        raise "Aborting reloading #{url} as a #{code} was returned"
+      end
+
+      status_codes.include?(code)
+    end
+
+    retry_while_false(
+      attempt_method: attempt_method,
+      fail_reason: "#{url} was not returning #{status_codes.join(',')}",
+      reload_seconds: reload_seconds,
+      interval_seconds: interval_seconds,
+    )
+  end
+
   ## Inspired by (/copied from) https://github.com/natritmeyer/site_prism/blob/master/lib/site_prism/waiter.rb
-  def reload_page_while_false(fail_reason: nil, reload_seconds: nil, interval_seconds: nil)
+  def retry_while_false(attempt_method:, retry_method: nil, fail_reason: nil, reload_seconds: nil, interval_seconds: nil)
     start_time = Time.now
     wait_time_seconds = reload_seconds || RSpec.configuration.reload_page_wait_time
     interval_seconds = interval_seconds || 0.5
     loop do
-      return true if yield
+      return true if attempt_method.call
       break unless Time.now - start_time <= wait_time_seconds
       sleep(interval_seconds)
-      visit(page.current_url)
+      retry_method.call if retry_method
     end
-    fail_reason ||= "#{page.current_url} was not passing the expectation."
+    fail_reason ||= "the expectation was not met"
     raise TimeoutError, "After #{wait_time_seconds} seconds, #{fail_reason}"
   end
 
-  RSpec.configuration.include ReloadPageHelpers
+  RSpec.configuration.include RetryHelpers
 end
