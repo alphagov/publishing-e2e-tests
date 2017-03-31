645ee8a8ec098bcc23a9295ae792fec01a0979ad
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-03-31 11:48:51 +0100

    Travel Advice Pulisher Refactored specs
    Tests from https://github.com/alphagov/publishing-e2e-tests/pull/20
    refactored to be consistent with the approach to writing tests used on
    specialist publisher.

---
+++ spec/travel_advice_publisher/creating_live_spec.rb
@@ -0,0 +1,50 @@
+feature "Creating a live edition on Travel Advice Publisher", feature: true, travel_advice_publisher: true do
+  include TravelAdvicePublisherHelpers
+
+  let(:country) { "Malta" }
+  let(:summary) { paragraph_with_timestamp }
+  let(:part_title) { title_with_timestamp }
+  let(:part_body) { Faker::Lorem.sentence }
+
+  scenario "Creating a live edition of Malta" do
+    when_i_create_a_live_edition_of_malta
+    then_i_can_view_it_on_gov_uk
+    and_i_can_view_parts_of_it
+  end
+
+  def when_i_create_a_live_edition_of_malta
+    visit_travel_advice_publisher("/admin")
+    click_link(country)
+
+    click_button("Create new edition")
+
+    fill_in_advice_form(
+      summary: summary,
+      parts: [
+        { title: part_title, body: part_body },
+      ]
+    )
+
+    click_button("Save & Publish")
+    expect_published_alert(country)
+  end
+
+  def then_i_can_view_it_on_gov_uk
+    url = find_link("view")[:href]
+    reload_url_until_status_code(url, 200)
+    reload_url_until_match(url, :has_text?, ignore_quotes_regex(summary))
+
+    @window = window_opened_by { click_link("view") }
+    within_window(@window) do
+      expect_rendering_application("government-frontend")
+      expect(page).to have_content(ignore_quotes_regex(summary))
+    end
+  end
+
+  def and_i_can_view_parts_of_it
+    within_window(@window) do
+      click_link(part_title)
+      expect(page).to have_content(part_body)
+    end
+  end
+end
