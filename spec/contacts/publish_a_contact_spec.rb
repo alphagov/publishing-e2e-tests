f8dc9b3769a0137986a5f7859c347cb75e9efdb2
Ryan MacGillivray <rmacg89@gmail.com>
2018-01-23 16:57:48 +0000

    Update publish test to cover finder frontend
    This provides confidence that published contacts are correctly rendered
    on the finder frontend for their organisations.

--- spec/contacts/publish_a_contact_spec.rb
+++ spec/contacts/publish_a_contact_spec.rb
@@ -1,25 +1,35 @@
-feature "Publishing a contact", contacts: true, new: true, government_frontend: true do
+feature "Publishing a contact", contacts: true, new: true, finder_frontend: true, government_frontend: true do
+  include ContactsHelpers
+
   let(:title) { "Creating a contact #{SecureRandom.uuid}" }
 
   scenario "Creating a contact" do
     when_i_create_a_contact
     then_i_can_view_it_on_gov_uk
+    and_i_can_view_it_on_finder
   end
 
   def when_i_create_a_contact
-    visit(Plek.find("contacts-admin") + "/admin/contacts/new")
-    fill_in "contact_title", with: title
-    fill_in "Description", with: sentence
-    click_button "Create Contact"
+    publish_contact(title: title)
   end
 
   def then_i_can_view_it_on_gov_uk
     url = find_link(title)[:href]
     reload_url_until_status_code(url, 200)
+    reload_url_until_match(url, :has_text?, "Contact HMRC")
 
     click_link title
     expect(page).to have_content(title)
     expect_url_matches_live_gov_uk
     expect_rendering_application("government-frontend")
+  end
+
+  def and_i_can_view_it_on_finder
+    contact_finder_url = find_link("Contact HMRC")[:href]
+    reload_url_until_match(contact_finder_url, :has_text?, title)
+
+    click_link "Contact HMRC"
+    expect_rendering_application("finder-frontend")
+    expect(page).to have_content(title)
   end
 end
