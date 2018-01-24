7b8d3c6f90e209aec56bcc43ef7a21a34a1e2d5b
Ryan MacGillivray <rmacg89@gmail.com>
2018-01-24 12:15:19 +0000

    Add test for removing a contact
    This provides us confidence that when a contact is removed in the admin
    the change is correctly reflected throughout the live stack.

---
+++ spec/contacts/removing_a_contact_spec.rb
@@ -0,0 +1,43 @@
+feature "Removing a contact", contacts: true, finder_frontend: true, government_frontend: true, new: true do
+  include ContactsHelpers
+
+  let(:title) { "Removing a contact #{SecureRandom.uuid}" }
+
+  scenario "Removing a contact" do
+    given_there_is_a_published_contact
+    when_i_remove_the_contact
+    then_i_get_a_410_on_gov_uk
+    and_it_is_not_shown_on_finder
+  end
+
+  private
+
+  def given_there_is_a_published_contact
+    publish_contact(title: title)
+    @url = find_link(title)[:href]
+  end
+
+  def when_i_remove_the_contact
+    search_for_contact(title: title)
+    click_link "Edit contact"
+
+    page.accept_confirm do
+      click_link "Delete"
+    end
+  end
+
+  def then_i_get_a_410_on_gov_uk
+    reload_url_until_status_code(@url, 410, keep_retrying_while: [404, 200])
+
+    visit(@url)
+    expect(page).to have_content("gone")
+  end
+
+  def and_it_is_not_shown_on_finder
+    contact_finder_url = Pathname.new(current_url).parent.to_s
+    reload_url_until_match(contact_finder_url, :has_no_text?, title)
+    visit contact_finder_url
+    expect_rendering_application("finder-frontend")
+    expect(page).to_not have_content(title)
+  end
+end
--- spec/support/contacts_helpers.rb
+++ spec/support/contacts_helpers.rb
@@ -5,4 +5,9 @@ module ContactsHelpers
     fill_in "Description", with: sentence
     click_button "Create Contact"
   end
+
+  def search_for_contact(title:)
+    fill_in "contacts_search_q", with: title
+    click_button "Filter contacts"
+  end
 end
