9c5a7bc4af6d48b7bb7a60f97500ca702c8ca45e
Adrian Clay <adrian@madetech.com>
2018-01-25 18:07:56 +0000

    Add test for attachments on Whitehall
    This gives confidence that Whitehall can communicate with
    asset-manager.

---
+++ spec/whitehall/uploading_attachment_spec.rb
@@ -0,0 +1,44 @@
+feature "Uploading an attachment on Whitehall", new: true, whitehall: true, government_frontend: true do
+  include WhitehallHelpers
+
+  let(:title) { "Attachment Whitehall #{SecureRandom.uuid}" }
+  let(:attachment_file) { File.expand_path("../fixtures/whitehall/public_health.png", __dir__) }
+  let(:attachment_alt_text) { "Public Health Attachment" }
+
+  scenario "Uploading an attachment on Whitehall" do
+    given_i_have_a_draft_document_with_attachment
+    when_i_view_the_draft_document
+    then_i_can_view_the_image
+  end
+
+  def given_i_have_a_draft_document_with_attachment
+    visit(Plek.find("whitehall-admin") + "/government/admin/consultations/new")
+    within(:css, ".file_upload") do
+      attach_file("File", attachment_file)
+      fill_in "Alt text", with: attachment_alt_text
+    end
+    image_markdown = "!!1"
+    fill_in_consultation_form(title: title, body: "Attached image\n\n#{image_markdown}")
+    click_button("Save")
+    expect(page).to have_text("The document has been saved")
+  end
+
+  def when_i_view_the_draft_document
+    url = find_link("Preview on website")[:href]
+    reload_url_until_status_code(url, 200)
+
+    switch_to_window(window_opened_by { click_link("Preview on website") })
+
+    expect_rendering_application("government-frontend")
+    expect_url_matches_draft_gov_uk
+    expect(page).to have_content(title)
+  end
+
+  def then_i_can_view_the_image
+    attachment_url = find(:xpath, "//img[@alt=\"#{attachment_alt_text}\"]")[:src]
+
+    # Asset manager returns a 302 to a placeholder image until the asset is available
+    reload_url_until_status_code(attachment_url, 200, keep_retrying_while: 302)
+    expect_matching_uploaded_file(attachment_url, attachment_file)
+  end
+end
