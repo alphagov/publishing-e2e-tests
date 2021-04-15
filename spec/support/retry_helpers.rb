41d8e145dbedb46825d1337669274207a9b82c4b
Kevin Dew <kevindew@me.com>
2021-04-15 19:07:04 +0100

    Automatically fix linting offences
    rubocop -A

--- spec/support/retry_helpers.rb
+++ spec/support/retry_helpers.rb
@@ -80,12 +80,12 @@ module RetryHelpers
     end
   end
 
-  alias expect_status_code_eventually reload_url_until_status_code
+  alias_method :expect_status_code_eventually, :reload_url_until_status_code
 
   def retry_while_false(fail_reason: nil, reload_seconds: nil, interval_seconds: nil, &block)
     reload_seconds ||= RSpec.configuration.reload_page_wait_time
     interval_seconds ||= 0.5
-    success = RetryWhileFalse.(reload_seconds: reload_seconds, interval_seconds: interval_seconds, &block)
+    success = RetryWhileFalse.call(reload_seconds: reload_seconds, interval_seconds: interval_seconds, &block)
     fail_reason ||= "the expectation was not met"
     raise TimeoutError, "After #{reload_seconds} seconds, #{fail_reason}" unless success
   end
