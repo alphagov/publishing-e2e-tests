c482dd9368ce11ba7b544df805ec1e18779ed076
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-10-06 12:53:20 +0100

    WIP: Try finding taxon by its label

--- spec/support/whitehall_helpers.rb
+++ spec/support/whitehall_helpers.rb
@@ -4,7 +4,7 @@ module WhitehallHelpers
     fill_in_consultation_form(title: title)
     click_button("Save and continue")
     expect(page).to have_text("The document has been saved")
-    check "Test taxon"
+    check "Test taxon", allow_label_click: true
     click_button("Save and review specialist topic tagging")
     expect(page).to have_text("The tags have been updated")
     click_button("Save")
