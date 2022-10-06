1b7fa9d024b3aaf79bd66280c30010d04efd8fa7
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-10-06 13:03:50 +0100

    Expect new button text
    The button text on the 'Taxonomy topic tags' page changed in alphagov/whitehall#6893.
    
    It used to be:
    > Save and review specialist topic tagging
    
    But is now:
    > Update and review specialist topic tags
    
    So I've updated the affected tests accordingly.

--- spec/whitehall/uploading_attachment_spec.rb
+++ spec/whitehall/uploading_attachment_spec.rb
@@ -28,7 +28,7 @@ feature "Uploading an attachment on Whitehall", whitehall: true, government_fron
     fill_in_consultation_form(title: title, body: "Attached image\n\n#{image_markdown}")
     click_button("Save and continue")
     check "Test taxon"
-    click_button("Save and review specialist topic tagging")
+    click_button("Update and review specialist topic tags")
     click_button("Save")
     expect(page).to have_text("The associations have been saved")
   end
